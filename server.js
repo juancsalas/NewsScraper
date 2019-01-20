c0f6deb8646a8ad474e9be87074a60a68a697f9e
Juan Carlos Salas <jcsalas08@gmail.com>
2019-01-19 20:52:01 -0800

    Added process.env.PORT

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const exphbs = require("express-handlebars");
 var app = express();
 
 var db = require("./models");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Setting Handlebars
 
